e4f27d25b1f4d9b0fc264d65b4fb709022ef2764
Mike Krzy <krzychusnk@gmail.com>
2020-04-06 11:50:47 +0200

    Render pass created with bounded depth attachment.

--- Vulkan_Tutorial/Vulkan_Tutorial/TutorialApp.cpp
+++ Vulkan_Tutorial/Vulkan_Tutorial/TutorialApp.cpp
@@ -304,6 +304,7 @@ void TutorialApp::createImageViews()
 
 void TutorialApp::createRenderPass()
 {
+    /* COLOR ATTACHMENT */
     VkAttachmentDescription colorAttachment = {};
     colorAttachment.format  = this->swapChainImageFormat;
     colorAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
@@ -318,10 +319,27 @@ void TutorialApp::createRenderPass()
     colorAttachmentRef.attachment   = 0;
     colorAttachmentRef.layout       = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
 
+    /* DEPTH ATTACHMENT */
+    VkAttachmentDescription depthAttachment = {};
+    depthAttachment.format  = this->findDepthFormat();
+    depthAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
+    depthAttachment.loadOp  = VK_ATTACHMENT_LOAD_OP_CLEAR;      /* Clear data in attachment before rendering. */
+    depthAttachment.storeOp = VK_ATTACHMENT_STORE_OP_DONT_CARE; /* Do not store depth data for now. */
+    depthAttachment.stencilLoadOp   = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+    depthAttachment.stencilStoreOp  = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+    depthAttachment.initialLayout   = VK_IMAGE_LAYOUT_UNDEFINED;
+    depthAttachment.finalLayout     = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
+    
+    VkAttachmentReference depthAttachmentRef = {};
+    depthAttachmentRef.attachment   = 0;
+    depthAttachmentRef.layout       = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
+    
+    /* Define subpass attachments */
     VkSubpassDescription subpass    = {};
     subpass.pipelineBindPoint       = VK_PIPELINE_BIND_POINT_GRAPHICS;
     subpass.colorAttachmentCount    = 1;
     subpass.pColorAttachments       = &colorAttachmentRef;
+    subpass.pDepthStencilAttachment = &depthAttachmentRef;
 
     /* Subpass dependencies */
     VkSubpassDependency dependency = {};
@@ -330,10 +348,12 @@ void TutorialApp::createRenderPass()
     dependency.srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
     dependency.dstStageMask = VK_ACCESS_COLOR_ATTACHMENT_READ_BIT | VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
 
+    /* Render pass consist of two attachment- color and depth. */
+    std::array<VkAttachmentDescription, 2> attachments = {colorAttachment, depthAttachment};
     VkRenderPassCreateInfo renderPassInfo = {};
     renderPassInfo.sType        = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
-    renderPassInfo.attachmentCount  = 1;
-    renderPassInfo.pAttachments     = &colorAttachment;
+    renderPassInfo.attachmentCount  = static_cast<uint32_t>(attachments.size());
+    renderPassInfo.pAttachments     = attachments.data();
     renderPassInfo.subpassCount     = 1;
     renderPassInfo.pSubpasses       = &subpass;
     renderPassInfo.dependencyCount  = 1;
